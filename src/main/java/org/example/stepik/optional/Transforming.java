29ed87638107c73fad1e0c04d515cf846d50847c
rafael-rezyapov <rezograf@gmail.com>
2023-02-14 18:22:18 +0300

    add Optional:engineNameToUppercase

--- src/main/java/org/example/stepik/optional/Transforming.java
+++ src/main/java/org/example/stepik/optional/Transforming.java
@@ -33,6 +33,29 @@ public class Transforming {
                     .flatMap(Engine::getName)
                     .map(n->n.toUpperCase());
     }
+/**
+ * 1.While Optional provides a lot of improvements, it's easy to misuse it.
+ * There are a few pieces of advice that can help you to avoid such mistakes:
+ * Avoid returning null. In most cases, the clients of your API expect that the response is safe.
+ * Returning null leads to NullPointerException, and it's a bad way to build your API.
+ * One option is to return the Optional.empty instead.
+ *
+ * 2.Don't use Optional as a method parameter. If the method has an Optional parameter,
+ * you have to do the unnecessary wrapping and pack an argument into Optional.
+ * It leads to low code readability. If the method parameter is optional to have,
+ * use overloading instead.
+ *
+ * 3.In most cases, it's more convenient to use the
+ * Optional.map and the Optional.flatMap
+ * instead of the Optional.ifPresent
+ * to avoid unnecessary if checks.
+ *
+ * 4.Since Optional is not serializable, don't use it for class fields.
+ * The primary design goal of Optional is to be used as the return value of functions
+ * when a return value might be absent.
+ *
+ * 5.Last but not least: keep it simple, don't overuse Optional.
+ */
 }
 
 class Engine {
